fe568af6b6a4bd7117859e093ab0ad6fe9cf229d
azizahnurrahma <azizahnurrahma06@gmail.com>
2026-04-17 06:55:57 +0700

    bikin fitur register dan login

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Jalur untuk Daftar Akun
+Route::post('/register', [AuthController::class, 'register']);
+
+// Jalur untuk Masuk
+Route::post('/login', [AuthController::class, 'login']);
